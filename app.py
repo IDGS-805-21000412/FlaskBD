e1555fe826e7484494093754cf3f6c8da2993739
IDGS-805-21000412 <nayepm301@gmail.com>
2025-03-07 01:17:44 -0600

    ABC Completo

--- app.py
+++ app.py
@@ -3,27 +3,31 @@ from flask import flash
 from flask_wtf.csrf import CSRFProtect
 from flask import g
 from config import DevelopmentConfig
-import forms
 
-from models import db, Alumnos
+from models import db
+from models import Alumnos
+
+import forms
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
-csrf = CSRFProtect(app)
+csrf=CSRFProtect()
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html'),404
 
-@app.route("/")
+
+@app.route("/",methods=['GET','POST'])
 @app.route("/index")
 def index():
     create_form=forms.UserForm2(request.form)
-
+    
     alumno=Alumnos.query.all()
-    return render_template("index.html",form=create_form,alumnos=alumno)
+    
+    return render_template("index.html", form=create_form,alumnos=alumno)
 
-@app.route("/detalles", methods=['GET', 'POST'])
+@app.route("/detalles",methods=['GET','POST'])
 def detalles():
     create_form=forms.UserForm2(request.form)
     if request.method == 'GET':
@@ -31,25 +35,69 @@ def detalles():
         alum1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
         nom=alum1.nombre
         ape=alum1.apaterno
-        email=alum1.email
-        return render_template("detalles.html",form=create_form,nombre=nom,apaterno=ape,email=email)
+        email=alum1.email        
+    return render_template("detalles.html", form=create_form,nombre=nom, apaterno=ape, email=email)
 
-@app.route('/Alumnos1',methods=['GET', 'POST'])
+@app.route("/Alumnos1",methods=['GET','POST'])
 def Alumnos1():
     create_form=forms.UserForm2(request.form)
-    if request.method=='POST':
+    if request.method == 'POST':
         alum=Alumnos(nombre=create_form.nombre.data,
             apaterno=create_form.apaterno.data,
             email=create_form.email.data)
-        
-        db.session.add(alum)
+        #insert alumnos() values()
+        db.session.add(alum) #es para agregar       
+        db.session.commit() #lo guarda permanentemente en la bd
+        return redirect(url_for('index'))
+    return render_template("Alumnos1.html", form=create_form)
+
+@app.route('/modificar',methods=['GET', 'POST'])
+def modificar():
+    create_form=forms.UserForm2(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=request.args.get('id')
+        create_form.nombre.data=str.rstrip(alum1.nombre)
+        create_form.apaterno.data=alum1.apaterno
+        create_form.email.data=alum1.email
+    if request.method=='POST':
+        id=create_form.id.data
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        alum1.id=id
+        alum1.nombre=str.rstrip(create_form.nombre.data)
+        alum1.apaterno=create_form.apaterno.data
+        alum1.email=create_form.email.data
+        db.session.add(alum1)
         db.session.commit()
         return redirect(url_for('index'))
-    return render_template('Alumnos1.html', form=create_form)
+    return render_template('modificar.html',form=create_form)
+
+@app.route('/eliminar',methods=['GET','POST'])
+def eliminar():
+    create_form=forms.UserForm2(request.form)
+    if request.method=='GET':
+        id=request.args.get('id')
+        # select * form alumnos where id == id
+        alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+        create_form.id.data=request.args.get('id')
+        create_form.nombre.data=alum1.nombre
+        create_form.apaterno.data=alum1.apaterno
+        create_form.email.data=alum1.email
+    if request.method=='POST':
+        id=create_form.id.data
+        alum = Alumnos.query.get(id)
+        #delete from alumnos where id == id
+        db.session.delete(alum)
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('eliminar.html',form=create_form)
+        
+        
 
 if __name__ == '__main__':
     csrf.init_app(app)
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    app.run()
+    app.run(debug=True, port=3000)
